4c3ddb27e61c55641fe58ad9337707e2a7d909a1
Charlie <cpenvari@uccs.edu>
2021-03-07 14:47:25 -0700

    extracted solve to func

--- python_sim_solv.py
+++ python_sim_solv.py
@@ -13,3 +13,19 @@ bounds = (0, None) # already default params, but specifying just for show
 soln = optimize.linprog(c=ob_func,A_ub=ub_coeff_matrix,b_ub=ub_rhs_matrix, method='simplex', bounds=bounds)
 print("Project Solve")
 print(soln)
+
+# Should assert:
+
+'''
+------------------------------------------------------------
+Project Solve
+     con: array([], dtype=float64)
+     fun: -40.5
+ message: 'Optimization terminated successfully.'
+     nit: 4
+   slack: array([0., 6.])
+  status: 0
+ success: True
+       x: array([0. , 4.5, 0. ])
+
+'''
